deb25162d598f62a6d08ea27401f542b24ac69fd
Tarak Nath Paul <passenger.code016@gmail.com>
2025-09-22 18:44:47 +0530

    fix(config): resolve duplicate declaration issue for WorkFlowClient
    - Instead of naming the imported Client as WorkFlowClient we just used it as Client

--- config/upstash.js
+++ config/upstash.js
@@ -1,11 +1,11 @@
-import { Client as WorkFlowClient } from '@upstash/workflow';
+import { Client } from '@upstash/workflow';
 import { QSTASH_TOKEN, QSTASH_URL } from '../config/env.js';
 
 // create a new instance of the Upstash WorkFlow client
 // this client will be used to interact with the Upstash WorkFlow service
 // we need to provide the baseUrl and token for authentication
 
-export const WorkFlowClient = new WorkFlowClient({
+export const WorkFlowClient = new Client({
     baseUrl: QSTASH_URL,
     token: QSTASH_TOKEN
 })
