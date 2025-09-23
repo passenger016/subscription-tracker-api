8f23bac5f7587e5f23d7d6a16816ea8764134445
Tarak Nath Paul <passenger.code016@gmail.com>
2025-09-23 16:22:14 +0530

    feat(config): add nodemailer configuration for Gmail SMTP
    - Added nodemailer setup in config/nodemailer.js to enable email sending via Gmail SMTP service.
    - Configured transporter with authentication and recommended debug practices for development.

---
+++ config/nodemailer.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+import { EMAIL_PASSWORD } from './env.js';
+
+// constant containing the email address of the sender
+export const accountEmail = 'passenger.code016@gmail.com'
+
+
+// we will configure a transporter using GMAIL as a service
+const transporter = nodemailer.createTransport({
+    service: 'Gmail', // ids are case-insensitive
+    auth: {
+        user: accountEmail,
+        pass: EMAIL_PASSWORD,
+    }
+});
+
+// a common resuable HTML template for the email body is created under /utils/email-template.js
+// '/utils' folder contains functions that can be reused across the application
