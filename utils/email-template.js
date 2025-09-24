3140c08f4814d1441b67391db9d0068a4ec9e20f
Tarak Nath Paul <passenger.code016@gmail.com>
2025-09-24 19:40:57 +0530

    chore(utils): update brand name to subtrack

--- utils/email-template.js
+++ utils/email-template.js
@@ -13,7 +13,7 @@ export const generateEmailTemplate = ({
     <table cellpadding="0" cellspacing="0" border="0" width="100%" style="background-color: #ffffff; border-radius: 10px; overflow: hidden; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
         <tr>
             <td style="background-color: #4a90e2; text-align: center;">
-                <p style="font-size: 54px; line-height: 54px; font-weight: 800;">SubDub</p>
+                <p style="font-size: 54px; line-height: 54px; font-weight: 800;">SubTrack</p>
             </td>
         </tr>
         <tr>
